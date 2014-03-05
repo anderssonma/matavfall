eff4fe9194ffb181b090ba524c43093fa56647ff
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-05 09:08:18 +0100

    Random delete bug?

--- Build/assets/js/1.8_recept.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('#open-recipes').on('click', function() {
-		$('.book-wrapper').addClass('open');
-	});
-	$('#close-book').on('click', function() {
-		$('.book-wrapper').removeClass('open');
-	});
-});
