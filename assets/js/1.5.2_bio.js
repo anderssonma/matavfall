55f54d4c0e5cbd688e47dd79405dd4bb8198752d
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-13 16:20:31 +0100

    BIO: Small JS optimizations and JSHinted

--- assets/js/1.5.2_bio.js
+++ assets/js/1.5.2_bio.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
 
-	// SMOOTH SCROLL IN WINDOWS TOO
-	// $('html').niceScroll();
-	var cinemaCtrl = $.superscrollorama();
-	var cinemaVideo = document.getElementById('video');
-	var cinemaPlayBtn = $('#play-btn');
-	var pauseVideo = function() {
-		if (!cinemaVideo.paused) {
-			cinemaVideo.pause();
-			cinemaPlayBtn.show();
-		}
-	};
+	var cinemaCtrl = $.superscrollorama(),
+			cinemaVideo = document.getElementById('video'),
+			cinemaPlayBtn = document.getElementById('play-btn'),
+			pauseVideo = function() {
+				if (!cinemaVideo.paused) {
+					cinemaVideo.pause();
+					cinemaPlayBtn.show();
+				}
+			};
 
 			// SEATS IN & OUT
 	var row1In = TweenMax.to($('#row1'), 2, {css:{bottom: '+=200'}, ease: Back.easeOut}),
@@ -21,13 +19,13 @@ $(document).ready(function() {
 			screenIn = TweenMax.to($('#interactive'), 3, {css:{top: 0}, ease: Back.easeOut}),
 			screenOut = TweenMax.to($('#interactive'), 3, {css:{top: 700}, ease: Back.easeIn}),
 			// CURTAINS IN & OUT - PAUSE THE VIDEO IF IT'S PLAYING ONCOMPLETE
-  		leftCurtainOut = TweenMax.to($('#curtain-left'), 5, {css:{left: -520}, ease:Back.easeIn}),
-  		rightCurtainOut = TweenMax.to($('#curtain-right'), 5, {css:{right: -520}, ease:Back.easeIn, onReverseComplete: pauseVideo}),
-  		leftCurtainIn = TweenMax.to($('#curtain-left'), 5, {css:{left: -100}, ease:Back.easeOut}),
-  		rightCurtainIn = TweenMax.to($('#curtain-right'), 5, {css:{right: -100}, ease:Back.easeOut, onComplete: pauseVideo}),
+			leftCurtainOut = TweenMax.to($('#curtain-left'), 5, {css:{left: -520}, ease:Back.easeIn}),
+			rightCurtainOut = TweenMax.to($('#curtain-right'), 5, {css:{right: -520}, ease:Back.easeIn, onReverseComplete: pauseVideo}),
+			leftCurtainIn = TweenMax.to($('#curtain-left'), 5, {css:{left: -100}, ease:Back.easeOut}),
+			rightCurtainIn = TweenMax.to($('#curtain-right'), 5, {css:{right: -100}, ease:Back.easeOut, onComplete: pauseVideo}),
 			// READ MORE IN & OUT
-  		signIn = TweenMax.to($('#sign'), 2, {css:{bottom: 40}, ease: Expo.easeOut}),
-  		signOut = TweenMax.to($('#sign'), 2, {css:{bottom: -200}, ease: Expo.easeIn});
+			signIn = TweenMax.to($('#sign'), 2, {css:{bottom: 40}, ease: Expo.easeOut}),
+			signOut = TweenMax.to($('#sign'), 2, {css:{bottom: -200}, ease: Expo.easeIn});
 
 	cinemaCtrl.pin($('#cinema'), 12000, {
 		anim: (new TimelineLite())
@@ -38,9 +36,14 @@ $(document).ready(function() {
 			.add(signOut, '+=5')
 			.add([leftCurtainIn, rightCurtainIn], '+=0.5', 'start')
 			.add(screenOut)
-			.add([row2Out, row1Out], '+=0.5', 'sequence'),
-		onPin: console.log('Pin'),
-		onUnpin: console.log('unpin')
+			.add([row2Out, row1Out], '+=0.5', 'sequence')
+	});
+
+	// REVERT TO START ON 'END'
+	$(cinemaVideo).on('ended onended', function() {
+		cinemaVideo.currentTime = 0;
+		cinemaVideo.pause();
+		cinemaPlayBtn.fadeIn(250);
 	});
 
 	// PAUSE VIDEO ON VIDEO CLICK
@@ -51,17 +54,11 @@ $(document).ready(function() {
 		}
 	});
 	// PLAY ON PLAY CLICK
-	cinemaPlayBtn.on('click', function() {
+	$(cinemaPlayBtn).on('click', function() {
 		if (cinemaVideo.paused) {
 			cinemaVideo.play();
 			$(this).fadeOut(250);
 		}
 	});
-
-	$(cinemaVideo).on('ended onended', function() {
-		cinemaVideo.currentTime = 0;
-		cinemaVideo.pause();
-		cinemaPlayBtn.fadeIn(250);
-	})
 
 });
