6b1d6159ca28f060b27e5d4c943406fd9e3ab627
Marcus Andersson <marcus@marcus-andersson.com>
2014-03-13 10:18:59 +0100

    Adds a tabbable interface for recipes

--- Build/assets/js/1.4_recept.js
+++ Build/assets/js/1.4_recept.js
@@ -5,4 +5,19 @@ $(function() {
 	$('#close-book').on('click', function() {
 		$('.book-wrapper').removeClass('open');
 	});
+
+	var recipeTabs = {
+		activeItem: 1,
+		init: function() {
+			$('#recipe-list .recipe-item').on('click', function() { // HIDE OLD ITEM FIRST
+				$('#recipe-list, #recipe-box').find("[data-id='" + recipeTabs.activeItem + "']").removeClass('active');
+				var self = this;
+				window.setTimeout(function() { // THEN SHOW NEW AFTER X MS
+					recipeTabs.activeItem = $(self).data('id');
+					$('#recipe-list, #recipe-box').find("[data-id='" + recipeTabs.activeItem + "']").addClass('active');
+				}, 350);
+			});
+		}
+	};
+	recipeTabs.init();
 });
