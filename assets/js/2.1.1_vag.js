8db560ea6a61d3ea24681177ddcf1da44bd7bd59
Marcus Andersson <marcus@marcus-andersson.com>
2014-06-03 14:52:10 +0200

    BEN 2, Fakta 1: Visual refinements & cleanup

--- assets/js/2.1.1_vag.js
+++ assets/js/2.1.1_vag.js
@@ -25,7 +25,10 @@ $(document).ready(function() {
 	var roadIn = TweenMax.to($('#road-bio'), 2, {css:{opacity: '1', top: '-100px'}, ease:Expo.easeOut});
 
 	var truckIn = TweenMax.to($('#waste-truck'), 2, {css:{top: '50%'}, ease:Expo.easeOut});
+	var truckLidIn = TweenMax.to($('#waste-truck-lid'), 2, {css:{top: '50%'}, ease:Expo.easeOut});
 	var text2In = TweenMax.to($('#text-twoboxes'), 2, {css:{top: '50%', opacity: 1}, ease:Expo.easeOut});
+
+	var truckLidOff = TweenMax.to($('#waste-truck-lid'), 2, {css:{opacity: 0}, ease:Expo.easeOut});
 
 	// PART X: DRIVE
 	// =============
@@ -33,6 +36,8 @@ $(document).ready(function() {
 	var house1Out = TweenMax.to($('#house-1'), 2, {css:{top: '-50%'}, ease:Expo.easeIn});
 	var caskBoxOut = TweenMax.to($('#matavfallstunna-container'), 2, {css:{bottom: '150%'}, ease:Expo.easeIn});
 	var text2Out = TweenMax.to($('#text-twoboxes'), 2, {css:{top: '-50%'}, ease:Expo.easeIn});
+	var firsIn = TweenMax.to($('#road-firs'), 2, {css:{bottom: '40%'}, delay: 2, ease:Expo.easeOut});
+	var cowIn = TweenMax.to($('#road-cow'), 2, {css:{bottom: '70%'}, delay: 1.8, ease:Expo.easeOut});
 	var factoryIn = TweenMax.to($('#bio-factory'), 2, {css:{bottom: 0}, delay: 2, ease:Expo.easeOut});
 
 	var truckOut = TweenMax.to($('#waste-truck'), 4, {css:{top: '100%'}, ease:Expo.easeOut});
@@ -43,9 +48,10 @@ $(document).ready(function() {
 			.add([hand1In, waste1In])
 			.add([hand1Out, waste1Drop, caskBox, caskLid])
 			.add([caskBoxSmall, caskLidClose, house1In, roadIn])
-			.add([truckIn, text2In])
+			.add([truckIn, truckLidIn, text2In])
+			.add(truckLidOff)
 			// PART X: DRIVE
-			.add([moveRoad, house1Out, caskBoxOut, text2Out, factoryIn])
+			.add([moveRoad, house1Out, caskBoxOut, text2Out, firsIn, cowIn, factoryIn])
 			.add([truckOut])
 	});
 
