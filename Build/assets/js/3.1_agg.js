4e1a77a1cab28831caaf0385983e7674c9ba9ffa
Marcus Andersson <marcus@marcus-andersson.com>
2014-06-11 23:28:38 +0200

    BEN 3, Fakta 1: Added and fine-tuned the text

--- Build/assets/js/3.1_agg.js
+++ Build/assets/js/3.1_agg.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	var ctrl = $.superscrollorama();
 
+	var textIntroOut = TweenMax.to($('#text-intro'), 0.5, {css: {opacity: 0}});
+	var textIntroBgOut = TweenMax.to($('#text-intro-bg'), 1, {css: {opacity: 0}});
+
 	var straw1Out = TweenMax.to($('#straw-1'), 0.5, {
 			css: {opacity: 0, marginTop: '+=200'}});
 	var straw2Out = TweenMax.to($('#straw-2'), 0.5, {
@@ -10,6 +13,12 @@ $(document).ready(function() {
 			css: {opacity: 0, marginTop: '-=200'}});
 	var crown2Out = TweenMax.to($('#crown-2'), 0.5, {
 			css: {opacity: 0, marginTop: '-=200'}});
+
+	var text1In = TweenMax.to($('#text-1'), 0.5, {css: {opacity: 1}});
+	var text1Out = TweenMax.to($('#text-1'), 0.5, {css: {opacity: 0}});
+
+	var text2In = TweenMax.to($('#text-2'), 0.5, {css: {opacity: 1}});
+	var text2Out = TweenMax.to($('#text-2'), 0.5, {css: {opacity: 0}});
 
 	var corn1 = TweenMax.to($('#corn-1'), 0.5, {
 		bezier: {
@@ -218,6 +227,9 @@ $(document).ready(function() {
 	var circle1 = TweenMax.to($('#circle-1'), 1, {css: {transform: 'scale(100)'}});
 	var circle2 = TweenMax.to($('#circle-2'), 1.25, {css: {transform: 'scale(80)'}, delay: 0.25});
 
+	var text3In = TweenMax.to($('#text-3'), 0.5, {css: {opacity: 1}});
+	var text3Out = TweenMax.to($('#text-3'), 0.5, {css: {opacity: 0}});
+
 	var chickenFarmOut = TweenMax.to($('#chicken-farm'), 1, {css: {bottom: '150%'}});
 	var chickenFarmBGOut = TweenMax.to($('#chicken-farm-bg'), 1, {css: {bottom: '150%'}});
 	var chickenLampOut = TweenMax.to($('#chicken-lamp'), 1, {css: {top: '-120%'}});
@@ -227,6 +239,9 @@ $(document).ready(function() {
 	var eggCarton = TweenMax.to($('#egg-carton'), 1, {css: {bottom: '50%'}});
 	var eggCartonTop = TweenMax.to($('#egg-carton-top'), 1, {css: {top: '50%'}});
 	var eggCartonLogo = TweenMax.to($('#egg-carton-logo'), 1, {css: {top: '50%'}});
+
+	var text4In = TweenMax.to($('#text-4'), 0.5, {css: {opacity: 1}});
+	var text4Out = TweenMax.to($('#text-4'), 0.5, {css: {opacity: 0}});
 
 	var chickenFloorOut = TweenMax.to($('#chicken-farm-floor'), 1, {css: {bottom: '120%', height: '150%'},
 		onStart: function() {
@@ -253,6 +268,8 @@ $(document).ready(function() {
 	var cloud3 = TweenMax.to($('#cloud-3'), 1, {css: {bottom: '80%'}});
 
 	var tarmac = TweenMax.to($('#tarmac'), 1, {css: {bottom: '-50%'}});
+	var text5In = TweenMax.to($('#text-5'), 0.5, {css: {opacity: 1}});
+	var text5Out = TweenMax.to($('#text-5'), 0.5, {css: {opacity: 0}});
 	var exhaustIn = TweenMax.to($('#lorry-exhaust'), 1, {css: {opacity: '1'}});
 
 	var cloudBox = TweenMax.to($('#cloud-box'), 3, {css: {left: '-100%'}, ease:Expo.easeInOut});
@@ -281,6 +298,7 @@ $(document).ready(function() {
 		}
 	});
 	var tarmacSilhouette2 = TweenMax.to($('#tarmac img'), 6, {css: {left: '-50%'}});
+	var text7Out = TweenMax.to($('#text-7'), 0.5, {css: {opacity: 0}});
 	//var carBrake = TweenMax.to($('#car'), 6, {css: {left: '35%'}});
 
 	var tarmacTransition = TweenMax.to($('#tarmac'), 1, {css: {bottom: '0', marginBottom: 0}});
@@ -337,6 +355,9 @@ $(document).ready(function() {
 	var eggCloud6In = TweenMax.to($('#eggcircle-6'), 1.5, {css: {top: '58px', left: '94px'}, delay: 0.5});
 	var eggCloud7In = TweenMax.to($('#eggcircle-7'), 1.6, {css: {top: '-2px', left: '154px'}, delay: 0.6});
 	var eggCloud8In = TweenMax.to($('#eggcircle-8'), 1.7, {css: {top: '58px', left: '-26px'}, delay: 0.7});
+
+	var text8In = TweenMax.to($('#text-8'), 0.5, {css: {opacity: 1}});
+	var text8Out = TweenMax.to($('#text-8'), 0.5, {css: {opacity: 0}});
 
 	var handIn = TweenMax.to($('#hand'), 1, {css: {right: '50%'},
 		onStart: function() {
@@ -405,26 +426,40 @@ $(document).ready(function() {
 
 	var eatEgg = TweenMax.to($('#egg-cover'), 1, {css: {opacity: '1'}});
 
-	// TEMPORARY SPACER //
-	var spacer = TweenMax.to($('body'), 5, {css: {opacity: '1'}});
-	// TEMPORARY SPACER //
+	var plateUp = TweenMax.to($('#plate'), 1, {css: {bottom: '65%', transform: 'scale(0.75)'}});
+	var eggUp = TweenMax.to($('#egg-boil'), 1, {css: {top: '35%', transform: 'scale(0.75)', marginTop: '-10px'}});
+	var eggCoverUp = TweenMax.to($('#egg-cover'), 1, {css: {top: '35%', transform: 'scale(0.75)', marginLeft: '-8px'}});
+
+	var text9In = TweenMax.to($('#text-9'), 0.5, {css: {opacity: 1}});
+	var text9Spacer = TweenMax.to($('#text-9'), 3, {css: {opacity: 1}});
 
 	ctrl.pin($('#eggs'), 50000, {
 		anim: (new TimelineLite())
+			// INTRO
+			.add(textIntroOut, '+=2')
+			.add(textIntroBgOut)
 			// PART 1
 			.add([straw1Out, crown1Out])
 			.add([straw2Out, crown2Out], '-=0.25')
+			.add(text1In)
+			.add(text1Out, '+=2')
 			.add([corn1, corn4, corn2, corn10, corn3, corn7, corn8, corn5, corn11, corn9, corn12, corn6])
+			.add(text2In)
+			.add(text2Out, '+=2')
 			.add(bgToGreen, '+=2')
 			.add([corn1Out, corn2Out, corn3Out, corn4Out, corn5Out, corn6Out, corn7Out, corn8Out, corn9Out, corn10Out, corn11Out, corn12Out, kamikazeHen])
 			.add([scaleHen, hideFeathers])
 			.add(bgCollapse)
 			// PART 2
 			.add([chickenFarm, chickenFarmBG, chickenEgg, chickenFloor])
+			.add(text3In)
 			.add(chickenLampIn)
 			.add([circle1, circle2])
+			.add(text3Out)
 			.add([chickenFarmOut, chickenFarmBGOut, chickenLampOut, chickenFloorFull, henOut])
 			.add(eggCarton)
+			.add(text4In)
+			.add(text4Out, '+=2')
 			.add([eggCartonTop, eggCartonLogo])
 			.add(chickenFloorOut)
 			.add(eggCartonBg)
@@ -434,10 +469,13 @@ $(document).ready(function() {
 			.add(truckBot)
 			.add(truckFront)
 			.add([cloud1, cloud2, cloud3, tarmac])
-			.add([cloudBox, tarmacSilhouette, exhaustIn])
+			.add(text5In)
+			.add([cloudBox, tarmacSilhouette, exhaustIn], '+=2')
+			.add(text5Out)
 			.add(signIn)
 			.add(carIn, '+=1')
 			.add([cloudBox2, tarmacSilhouette2], '+=1')
+			.add(text7Out)
 			.add([tarmacTransition, cloud4Out, cloud5Out, cloud6Out])
 			// PART 4
 			.add([wheel1, wheel2, carBg1])
@@ -447,6 +485,8 @@ $(document).ready(function() {
 			.add([carBg4, carBgExpand, carCenter])
 			.add(carBgEggs)
 			.add([wheel1Carton, wheel2Carton, eggCloud1In, eggCloud2In, eggCloud3In, eggCloud4In, eggCloud5In, eggCloud6In, eggCloud7In, eggCloud8In])
+			.add(text8In)
+			.add(text8Out, '+=2')
 			.add(handIn)
 			.add([carBoxOut, tarmacOut]) //, eggCenter, handCenter])
 			// PART 5
@@ -460,7 +500,9 @@ $(document).ready(function() {
 			.add([eggToss, eggTossYolk, stoveOut, saucepanOut, spatulaToss, spatulaOut])
 			.add([eggTossBack, eggTossYolkBack, bg2Collapse, plateIn])
 			.add(eatEgg)
-			.add(spacer)
+			.add([plateUp, eggUp, eggCoverUp])
+			.add(text9In)
+			.add(text9Spacer)
 	});
 
 });
