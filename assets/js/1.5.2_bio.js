af73757acd69428200c770f169ae9a57116f916e
Marcus Andersson <marcus@marcus-andersson.com>
2014-07-02 09:28:28 +0200

    BEN 1, Bio: New fancy intro animations

--- assets/js/1.5.2_bio.js
+++ assets/js/1.5.2_bio.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 			cinemaVideo = document.getElementById('cinema-video'),
 			cinemaPlayOverlay = document.getElementById('play-overlay'),
 			cinemaMuteBtn = document.getElementById('mute-btn'),
+			cinema = $('#cinema'),
 			pauseVideo = function() {
 				if (!cinemaVideo.paused) {
 					cinemaVideo.pause();
@@ -12,20 +13,42 @@ $(document).ready(function() {
 			};
 
 			// SEATS IN & OUT
-	var row1In = TweenMax.to($('#row1'), 2, {css:{bottom: '+=200'}, ease: Back.easeOut}),
+	var row1In = TweenMax.to($('#row1'), 2, {css:{bottom: '+=200'}, ease: Back.easeOut, onReverseComplete: function() {
+				cinema.removeClass('lights-on search-on darken');
+			}}),
 			row2In = TweenMax.to($('#row2'), 2, {css:{bottom: '+=200'}, ease: Back.easeOut}),
 			row1Out = TweenMax.to($('#row1'), 2, {css:{bottom: '-=200'}, ease: Back.easeIn}),
 			row2Out = TweenMax.to($('#row2'), 2, {css:{bottom: '-=200'}, ease: Back.easeIn}),
 			// SCREEN IN & OUT
-			screenIn = TweenMax.to($('#interactive'), 3, {css:{top: 0}, ease: Back.easeOut}),
+			screenIn = TweenMax.to($('#interactive'), 3, {css:{top: 0}, ease: Back.easeOut, onComplete: function() {
+				cinema.addClass('lights-on search-on darken');
+				window.setTimeout(function() {
+					cinema.removeClass('lights-on');
+					window.setTimeout(function() {
+						cinema.addClass('lights-on');
+							window.setTimeout(function() {
+								cinema.removeClass('lights-on');
+								window.setTimeout(function() {
+									cinema.addClass('lights-on');
+									window.setTimeout(function() {
+										cinema.removeClass('search-on');
+									}, 3500);
+								}, 1300);
+							}, 1300);
+					}, 1300);
+				}, 1300);
+			}}),
 			screenOut = TweenMax.to($('#interactive'), 3, {css:{top: 700}, ease: Back.easeIn}),
 			// CURTAINS IN & OUT - PAUSE THE VIDEO IF IT'S PLAYING ONCOMPLETE
 			leftCurtainOut = TweenMax.to($('#curtain-left'), 5, {css:{left: -520}, ease:Back.easeIn}),
 			rightCurtainOut = TweenMax.to($('#curtain-right'), 5, {css:{right: -520}, ease:Back.easeIn, onReverseComplete: pauseVideo}),
 			leftCurtainIn = TweenMax.to($('#curtain-left'), 5, {css:{left: -100}, ease:Back.easeOut}),
-			rightCurtainIn = TweenMax.to($('#curtain-right'), 5, {css:{right: -100}, ease:Back.easeOut, onComplete: pauseVideo});
+			rightCurtainIn = TweenMax.to($('#curtain-right'), 5, {css:{right: -100}, ease:Back.easeOut, onComplete: function() {
+				pauseVideo();
+				cinema.removeClass('lights-on search-on darken');
+			}});
 
-	cinemaCtrl.pin($('#cinema'), 12000, {
+	cinemaCtrl.pin(cinema, 12000, {
 		anim: (new TimelineLite())
 			.add([row1In, row2In], '+=0.5', 'sequence')
 			.add(screenIn)
