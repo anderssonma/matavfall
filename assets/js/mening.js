1dbaf6523b186f7b601b51967f7091cd1e940ed1
Marcus Andersson <marcus@marcus-andersson.com>
2014-07-04 12:56:13 +0200

    Wordbuilder: initial commit

---
+++ assets/js/mening.js
@@ -0,0 +1,213 @@
+// CLASSLIST POLYFILL
+(function () {
+
+if (typeof window.Element === "undefined" || "classList" in document.documentElement) return;
+
+var prototype = Array.prototype,
+    push = prototype.push,
+    splice = prototype.splice,
+    join = prototype.join;
+
+function DOMTokenList(el) {
+  this.el = el;
+  // The className needs to be trimmed and split on whitespace
+  // to retrieve a list of classes.
+  var classes = el.className.replace(/^\s+|\s+$/g,'').split(/\s+/);
+  for (var i = 0; i < classes.length; i++) {
+    push.call(this, classes[i]);
+  }
+};
+
+DOMTokenList.prototype = {
+  add: function(token) {
+    if(this.contains(token)) return;
+    push.call(this, token);
+    this.el.className = this.toString();
+  },
+  contains: function(token) {
+    return this.el.className.indexOf(token) != -1;
+  },
+  item: function(index) {
+    return this[index] || null;
+  },
+  remove: function(token) {
+    if (!this.contains(token)) return;
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == token) break;
+    }
+    splice.call(this, i, 1);
+    this.el.className = this.toString();
+  },
+  toString: function() {
+    return join.call(this, ' ');
+  },
+  toggle: function(token) {
+    if (!this.contains(token)) {
+      this.add(token);
+    } else {
+      this.remove(token);
+    }
+
+    return this.contains(token);
+  }
+};
+
+window.DOMTokenList = DOMTokenList;
+
+function defineElementGetter (obj, prop, getter) {
+    if (Object.defineProperty) {
+        Object.defineProperty(obj, prop,{
+            get : getter
+        });
+    } else {
+        obj.__defineGetter__(prop, getter);
+    }
+}
+
+defineElementGetter(Element.prototype, 'classList', function () {
+  return new DOMTokenList(this);
+});
+
+})();
+
+// POINTER EVENTS POLYFILL
+/*
+ * Pointer Events Polyfill: Adds support for the style attribute "pointer-events: none" to browsers without this feature (namely, IE).
+ * (c) 2013, Kent Mewhort, licensed under BSD. See LICENSE.txt for details.
+ */
+
+// constructor
+function PointerEventsPolyfill(options){
+    // set defaults
+    this.options = {
+        selector: '*',
+        mouseEvents: ['click','dblclick','mousedown','mouseup'],
+        usePolyfillIf: function(){
+            if(navigator.appName == 'Microsoft Internet Explorer')
+            {
+                var agent = navigator.userAgent;
+                if (agent.match(/MSIE ([0-9]{1,}[\.0-9]{0,})/) != null){
+                    var version = parseFloat( RegExp.$1 );
+                    if(version < 11)
+                      return true;
+                }
+            }
+            return false;
+        }
+    };
+    if(options){
+        var obj = this;
+        $.each(options, function(k,v){
+          obj.options[k] = v;
+        });
+    }
+
+    if(this.options.usePolyfillIf())
+      this.register_mouse_events();
+}
+
+// singleton initializer
+PointerEventsPolyfill.initialize = function(options){
+    if(PointerEventsPolyfill.singleton == null)
+      PointerEventsPolyfill.singleton = new PointerEventsPolyfill(options);
+    return PointerEventsPolyfill.singleton;
+};
+
+// handle mouse events w/ support for pointer-events: none
+PointerEventsPolyfill.prototype.register_mouse_events = function(){
+    // register on all elements (and all future elements) matching the selector
+    $(document).on(this.options.mouseEvents.join(" "), this.options.selector, function(e){
+       if($(this).css('pointer-events') == 'none'){
+             // peak at the element below
+             var origDisplayAttribute = $(this).css('display');
+             $(this).css('display','none');
+
+             var underneathElem = document.elementFromPoint(e.clientX, e.clientY);
+
+            if(origDisplayAttribute)
+                $(this)
+                    .css('display', origDisplayAttribute);
+            else
+                $(this).css('display','');
+
+             // fire the mouse event on the element below
+            e.target = underneathElem;
+            $(underneathElem).trigger(e);
+
+            return false;
+        }
+        return true;
+    });
+};
+
+
+
+var SB = {
+
+	forceReflow: function() {
+		this.elList.offsetHeight;
+	},
+
+	pickWord: function(i) {
+		this.elList.classList.remove('show');
+
+		var choice = this.wordChoices[this.currentSet][i];
+		var item = document.getElementById('part-' + (this.currentSet + 1));
+
+		window.setTimeout(function() {
+			var spanClass = 'part-' + (SB.currentSet + 1) + '-span';
+			item.innerHTML = item.innerHTML.replace(/…/g, '') + ' <span id="' + spanClass + '">' + choice.replace(/…/g, '') + '</span>';
+			SB.forceReflow();
+			//$('#part-1 span').addClass('show');
+			document.getElementById(spanClass).classList.add('show');
+			$('body').addClass('part-2');
+			window.setTimeout(function() {
+				SB.currentSet++;
+				SB.nextSentence();
+			}, 1500)
+		}, 250);
+		
+	},
+	populateChoices: function() {
+		var htmlString = '';
+		var listItems = this.wordChoices[this.currentSet];
+		listItems.forEach(function(item, i) {
+			htmlString = htmlString + '<li onclick="SB.pickWord(' + i + ')">' + item + '</li>';
+		});
+		this.elList.innerHTML = htmlString;
+		this.forceReflow();
+		this.elList.classList.add('show');
+		
+	},
+
+	nextSentence: function() {
+
+		if (this.currentSet === this.wordChoices.length) {
+			// SHOW END SCREEN
+			return false;
+		}
+
+		var el = document.getElementById('part-' + (this.currentSet + 1));
+		el.classList.add('show');
+		this.forceReflow();
+		el.classList.add('in');
+
+		window.setTimeout(function() {
+			SB.populateChoices();
+		}, 250);
+		
+	},
+
+	init: function() {
+		this.wordChoices = wordChoices;
+		this.currentSet = 0;
+
+		this.elList = document.getElementById('sentence-choices');
+
+		this.populateChoices();
+	}
+};
+
+$(document).ready(function() {
+	SB.init();
+});
---
+++ assets/js/mening_data.js
@@ -0,0 +1,38 @@
+var wordChoices = [
+	[ // PART 1
+		'…och låter det rinna någon minut tills det blir lagom varmt.',
+		'…och tvättar mig, men står kvar 5 minuter och låter duschvärmen göra mig pigg.',
+		'…och duschar snabbt, som jag gör varje dag.',
+		'…och tar en dusch, men inte varje dag. Ibland räcker det med att tvätta sig ordentligt i handfatet.'
+	], [ // PART 3
+		'Jag gillar varmt bröd och värmer alltid brödet i micron eller ugnen.',
+		'Jag ber mamma sätta på spisen och steka ett ägg. Ibland får hon steka två gånger.',
+		'Jag äter en tallrik ekologisk filmjölk.',
+		'Allt på vårt frukostbord är ekologiskt och jag äter olika varje dag.'
+	], [ // PART 3
+		'…ta med dem när jag går hemifrån. Vi sorterar inte. Jag slänger allt bland restavfall.',
+		'…sortera men ibland glömmer jag bort mig och slänger allt bland restavfall ändå.',
+		'…lägga soporna där de ska vara, rätt sorterade.',
+		'…påminna alla i familjen när det är min vecka om att det är viktigt att sortera.'
+	], [ // PART 4
+		'…ber jag mamma eller pappa skjutsa mig till skolan, eller betala en taxi för mig.',
+		'…ber jag mamma eller pappa skjutsa mig till bussen, även om det är en omväg.',
+		'…tar jag bussen, men om det regnar eller snöar ber jag efter skjuts till busshållplatsen.',
+		'…cyklar jag till skolan, oavsett väder. Det är ju inte så långt!'
+	], [ // PART 5
+		'…godis! Och chips. Inget annat.',
+		'…helst frukt. Inte så noga, bara det mättar.',
+		'…nästan alltid en banan. Oftast är den ekologiskt odlad.',
+		'…en frukt som är ekologiskt odlad. Vi handlar bara sådana frukter hemma.'
+	], [ // PART 6
+		'…tänder alla lampor i hemmet och sätter på datorn, TV:n och stereon som sällskap.',
+		'…går till mitt rum och sätter igång datorn (som jag stängde av i morse).',
+		'…äter något lätt, gärna filmjölk, sedan gör jag läxor om jag har några.',
+		'…gör en rolig frågesport om miljöfrågor, som familjen får tävla om efter middagen.'
+	], [ // PART 7
+		'…har jag alltid en lampa tänd i rummet. Och stereon på, fast lågt. Då sover jag bäst.',
+		'…har jag datorn eller telefonen på hela natten. Man vet aldrig...',
+		'…har jag surfat tills jag blivit jättetrött. Men jag stänger av dator och telefon när jag ska sova.',
+		'…vill jag ha lugn och ro. Så jag släcker lamporna, dator och telefon.'
+	]
+];
