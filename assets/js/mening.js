ed21ed70861815c90d7021ebec1773d2eac4dd60
Marcus Andersson <marcus@marcus-andersson.com>
2014-07-11 10:59:39 +0200

    DAYBUILDER: Dynamic end screen and restart button

--- assets/js/mening.js
+++ assets/js/mening.js
@@ -1,82 +1,7 @@
-// CLASSLIST POLYFILL
-/*
-(function () {
-
-if (typeof window.Element === "undefined" || "classList" in document.documentElement) return;
-
-var prototype = Array.prototype,
-    push = prototype.push,
-    splice = prototype.splice,
-    join = prototype.join;
-
-function DOMTokenList(el) {
-  this.el = el;
-  // The className needs to be trimmed and split on whitespace
-  // to retrieve a list of classes.
-  var classes = el.className.replace(/^\s+|\s+$/g,'').split(/\s+/);
-  for (var i = 0; i < classes.length; i++) {
-    push.call(this, classes[i]);
-  }
-};
-
-DOMTokenList.prototype = {
-  add: function(token) {
-    if(this.contains(token)) return;
-    push.call(this, token);
-    this.el.className = this.toString();
-  },
-  contains: function(token) {
-    return this.el.className.indexOf(token) != -1;
-  },
-  item: function(index) {
-    return this[index] || null;
-  },
-  remove: function(token) {
-    if (!this.contains(token)) return;
-    for (var i = 0; i < this.length; i++) {
-      if (this[i] == token) break;
-    }
-    splice.call(this, i, 1);
-    this.el.className = this.toString();
-  },
-  toString: function() {
-    return join.call(this, ' ');
-  },
-  toggle: function(token) {
-    if (!this.contains(token)) {
-      this.add(token);
-    } else {
-      this.remove(token);
-    }
-
-    return this.contains(token);
-  }
-};
-
-window.DOMTokenList = DOMTokenList;
-
-function defineElementGetter (obj, prop, getter) {
-    if (Object.defineProperty) {
-        Object.defineProperty(obj, prop,{
-            get : getter
-        });
-    } else {
-        obj.__defineGetter__(prop, getter);
-    }
-}
-
-defineElementGetter(Element.prototype, 'classList', function () {
-  return new DOMTokenList(this);
-});
-
-})();
-*/
-
 var SB = {
-	score: 0,
 	scoreTable: [],
 	forceReflow: function() {
-		this.elList.offsetHeight;
+		this.$elList[0].offsetHeight;
 	},
 
 	insertInProgress: false,
@@ -88,9 +13,9 @@ var SB = {
 
 		this.scoreTable[this.currentSet + 1] = (i * 5);
 
-		var bigWord = $('#part-' + (this.currentSet + 1));
-		bigWord.removeClass('in');
-		$(this.elList).removeClass('show');
+		var $bigWord = $('#part-' + (this.currentSet + 1), this.$el);
+		$bigWord.removeClass('in');
+		this.$elList.removeClass('show');
 
 		/*
 		var choice = this.wordChoices[this.currentSet][i];
@@ -111,13 +36,13 @@ var SB = {
 		*/
 
 		window.setTimeout(function() {
-			$('.daybuilder').addClass('step-' + (SB.currentSet + 2));
+			SB.$el.addClass('step-' + (SB.currentSet + 2));
 			window.setTimeout(function() {
-				bigWord.removeClass('show');
+				$bigWord.removeClass('show');
 				// SHOW CLOCK
 				SB.currentSet++;
 				SB.nextSentence(1);
-			}, 1250);
+			}, 500);
 		}, 250);
 
 	},
@@ -127,9 +52,9 @@ var SB = {
 		listItems.forEach(function(item, i) {
 			htmlString = htmlString + '<li onclick="SB.pickWord(' + i + ')">' + item + '</li>';
 		});
-		this.elList.innerHTML = htmlString + '<li class="undo-button" onclick="SB.undo();">&larr;</li>';
+		this.$elList.html(htmlString + '<li class="undo-button" onclick="SB.undo();">&larr;</li>');
 		this.forceReflow();
-		$(this.elList).addClass('show');
+		this.$elList.addClass('show');
 	},
 
 	nextSentence: function(nextIndex) {
@@ -140,10 +65,10 @@ var SB = {
 			return false;
 		}
 
-		var el = $('#part-' + (this.currentSet + nextIndex));
-		el.addClass('show');
+		var $elPart = $('#part-' + (this.currentSet + nextIndex), this.$el);
+		$elPart.addClass('show');
 		this.forceReflow();
-		el.addClass('in');
+		$elPart.addClass('in');
 
 		window.setTimeout(function() {
 			SB.populateChoices();
@@ -162,11 +87,11 @@ var SB = {
 		this.scoreTable[this.currentSet] = 0;
 		var currentSet = this.currentSet + 1;
 
-		$(this.elList).removeClass('show');
-		$('#daybuilder').removeClass('step-' + currentSet);
-		$('#daybuilder #part-' + currentSet).removeClass('in');
+		this.$elList.removeClass('show');
+		this.$el.removeClass('step-' + currentSet);
+		$('#part-' + currentSet, this.$el).removeClass('in');
 		window.setTimeout(function() {
-			$('#daybuilder #part-' + currentSet).removeClass('show');
+			$('#part-' + currentSet, this.$el).removeClass('show');
 			SB.nextSentence(0);
 			SB.currentSet--;
 			SB.undoInProgress = false;
@@ -181,36 +106,60 @@ var SB = {
 			finalScore += points;
 		});
 
-		var title, imageSrc, textVerdict;
+		var title, imageSrc, textVerdict, classVerdict;
 		if (finalScore >= Math.floor(maxScore * 0.8)) { // 80% or more correct
 			title = 'MILJÖKÄMPEN';
-			imageSrc = 'jordglob_bra';
-			textVerdict = 'BRA JOBB'
+			imageSrc = 'jorden_glad';
+			textVerdict = 'Ett miljöhelgon både tänker rätt och gör rätt för en bättre miljö. Grattis! Fortsätt att tänka på hur du kan bidra till en bättre miljö, varje dag. Uppmana dina föräldrar att också bli miljöhelgon. Säg till din lärare att det är bra om ni alla tillsammans pratar om hur viktigt det är att välja ekologisk mat – hemma och i skolan.';
+			classVerdict = 'final-good';
 		} else if (finalScore >= Math.floor(maxScore * 0.4)) { // 40%-80% correct
 			title = 'MILJÖTALANGEN';
-			imageSrc = 'jordglob_neutral';
-			textVerdict = 'LAGOM JOBB';
+			imageSrc = 'jorden_neutral';
+			textVerdict = 'En miljötalang tänker på miljön ibland, men inte alltid. Du kan med enkla medel bli ett ”miljöhelgon”. Ta gärna hjälp av dina föräldrar. Be att de handlar mer ekologisk mat hemma. Hjälps åt att sortera avfall. Prata mer om hur ni alla, även mamma och pappa, kan förbättra miljön. Det får vi alla glädje av.';
+			classVerdict = 'final-okay';
 		} else {
 			title = 'MILJÖBOVEN';
-			imageSrc = 'jordglob_dalig';
-			textVerdict = 'DÅLIGT JOBB';
+			imageSrc = 'jorden_dyster';
+			textVerdict = 'En miljöbov bryr sig inte om att tänka på miljön över huvud taget. Du kan med enkla medel bli en ”miljötalang”. Spara energi genom att inte duscha onödigt länge, stäng av telefon, dator, tv och lampor när de inte används. Och hjälp både dig själv och andra med att sortera avfallet hemma.';
+			classVerdict = 'final-bad';
 		}
 
-		// SET FINAL IMAGE
-		// SET FINAL TITLE
-		// SET FINAL TEXT VERDICT
-		// THEN SHOW
-		$('.daybuilder').addClass('final');
+		$('.diploma img', this.$el).attr('src', '/assets/img/' + imageSrc + '.svg');
+		$('.diploma h2', this.$el).text(title);
+		$('.diploma p', this.$el).text(textVerdict);
+		window.setTimeout(function() {
+			SB.$el.addClass(classVerdict);
+		}, 250);
 
 		localStorage.setItem('SMM_DAYBUILDER_TITLE', title);
 		localStorage.setItem('SMM_DAYBUILDER_POINTS', finalScore);
+	},
+
+	restart: function() {
+		var $titles = $('.buildarea p', this.$el),
+				$choices = $('#sentence-choices', this.$el);
+
+		this.$el.attr('class', 'daybuilder step-1');
+		$titles.removeClass('in');
+		$choices.removeClass('show');
+		window.setTimeout(function() {
+			$titles.removeClass('show');
+			$choices.empty();
+			SB.scoreTable.length = 0;
+			SB.init();
+		}, 500);
 	},
 
 	init: function() {
 		//wordChoices.length = 2;
 		this.wordChoices = wordChoices;
 		this.currentSet = 0;
-		this.elList = document.getElementById('sentence-choices');
+		this.$el = $('#daybuilder');
+		this.$elList = $('#sentence-choices', this.$el);
+
+		$('.buildarea #part-1', this.$el).addClass('show');
+		this.forceReflow();
+		$('.buildarea #part-1', this.$el).addClass('in');
 		this.populateChoices();
 	}
 };
